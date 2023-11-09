75940d24404111a0f10e83206e3d8463979cf9c5
AhslaTano <74374643+AhslaTano@users.noreply.github.com>
2023-11-09 15:48:54 -0500

    Server.java

--- Server.java
+++ Server.java
@@ -32,17 +32,44 @@ public class Server {
             //TODO: implement main menu
             out.writeUTF("Welcome!");
             out.writeUTF(sendMenu(0));
-            Event event = new Event("Test", "Test", "11/20", 10, 20);
-            System.out.println(event.seats.availableSeats());
             //TODO: view events function
             //TODO: Buy a ticket function
             //TODO: seat selecting from string
             //TODO:View purchased tickets function
             
             String line = "";
+            boolean login = false;
+            while(!login){
+                try{
+                    System.out.print("Login (Username Password): ");
+                    line = in.readUTF();
+                    for(int i = 0; i < users.length; i++){
+                        if(users.ElementAt(i).getLogin().equals(line)){
+                            login = true;
+                    }else
+                        System.out.println("Wrong login, try again");
+                    }
+                }
+                catch(IOException i){
+                    System.out.println(i);
+                }
+            //Runtime input for the website
             while(!line.equals("quit")){
                 try{
+                    System.out.println("Enter a number for your option.\nMenu: " + sendMenu(0));
                     line = in.readUTF();
+                    switch(line){
+                        case "1":
+                            break;
+                        case "2":
+                            break;
+                        case "3":
+                            break;
+                        case "4":
+                            break;
+                        default:
+                            System.out.println("Error, Unkown command");
+                    }
                     System.out.println("Client: " + line);
                     String response = "Echo - " + line;
                     out.writeUTF(response);
@@ -68,7 +95,6 @@ public class Server {
         menus[0] = "Main menu:\n1.View Events\n2.Buy a Ticket\n3.View Purchased Tickets\n4.Quit";
         return menus[menuNumber];
     }
-    
 
     public static void main(String[] args){
         Server server = new Server(10000);    
